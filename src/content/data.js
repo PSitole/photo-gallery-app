dd78c86954caaa164044313f3433aeaa7b8422d6
Princess <psitole021@student.wethinkcode.co.za>
2023-06-17 13:51:42 +0200

    first react app

---
+++ src/content/data.js
@@ -0,0 +1,65 @@
+export const data = [
+  {
+    "name": "Anie",
+    "url": "/images/anie.jpeg",
+    "description": "Anime",
+    "date": "2023-06-17",
+    "type": "image/jpeg",
+  },
+  {
+    "name": "AnimeGuy",
+    "url": "/images/animeboy.jpeg",
+    "description": "Anime",
+    "date": "2023-06-17",
+    "type": "image/jpeg",
+  },
+  {
+    "name": "Kakashi",
+    "url": "/images/Kakashi.jpeg",
+    "description": "Anime",
+    "date": "2023-06-17",
+    "type": "image/jpeg",
+  },
+  {
+    "name": "Naruto",
+    "url": "/images/naruto.jpeg",
+    "description": "Anime",
+    "date": "2023-06-17",
+    "type": "image/jpeg",
+  },
+  {
+    "name": "Rias",
+    "url": "/images/Rias.jpeg",
+    "description": "Anime",
+    "date": "2023-06-17",
+    "type": "image/jpeg",
+  },
+  {
+    "name": "Chicken",
+    "url": "/images/chicken.jpg",
+    "description": "random chicken",
+    "date": "2023-06-17",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Cheese",
+    "url": "/images/chicken.jpg",
+    "description": "I Love Cheese",
+    "date": "2023-06-17",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Goku",
+    "url": "/images/Goku.jpg",
+    "description": "Anime",
+    "date": "2023-06-17",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Sakura",
+    "url": "/images/Sakura.jpg",
+    "description": "Anime",
+    "date": "2023-06-17",
+    "type": "image/jpg",
+  }
+]
